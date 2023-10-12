7775d129bdb1bfc63cf3cdc9aebde5c5fc65f45e
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-10-12 10:56:12 -0700

    complete

---
+++ shopping.js
@@ -0,0 +1,6 @@
+function showData(){
+    document.getElementById('abc').innerHTML='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus labore quaerat iste libero vitae, minima soluta? Quia iure ipsam commodi, odio tenetur sapiente, qui eaque autem, natus sunt voluptates repudiandae.'
+    a.innerText="Back";
+    const a = document.getElementById('read').style.display="none"
+ 
+}
