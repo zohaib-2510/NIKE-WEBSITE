c7e2297cc2a5b53fdf7e228440121b64fd2f4e3f
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-10-12 09:42:59 -0700

    NEW COLLection portion complete

---
+++ newcollection.js
@@ -0,0 +1,12 @@
+var btn1= document.getElementById("btn")
+btn1.addEventListener("click",function(){
+    btn1.innerHTML=("22/4/22")
+},500)
+var btn2= document.getElementById("btn1")
+btn2.addEventListener("click",function(){
+    btn2.innerHTML=("10/8/24")
+},500)
+var btn3= document.getElementById("btn2")
+btn3.addEventListener("click",function(){
+    btn3.innerHTML=("20/1/28")
+},500)
